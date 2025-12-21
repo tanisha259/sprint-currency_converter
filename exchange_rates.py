cff155645da1de963516488c7f997ab78a2cfb2c
Tanisha pandit <142971054+tanisha259@users.noreply.github.com>
2025-12-21 20:10:05 +0530

    Create exchange_rates.py

---
+++ exchange_rates.py
@@ -0,0 +1,22 @@
+exchange_rates = [
+    {"from": "USD", "to": "INR", "rate": 83.00},
+    {"from": "INR", "to": "USD", "rate": 0.012},
+
+    {"from": "USD", "to": "EUR", "rate": 0.92},
+    {"from": "EUR", "to": "USD", "rate": 1.08},
+
+    {"from": "USD", "to": "GBP", "rate": 0.79},
+    {"from": "GBP", "to": "USD", "rate": 1.27},
+
+    {"from": "USD", "to": "SGD", "rate": 1.35},
+    {"from": "SGD", "to": "USD", "rate": 0.74},
+
+    {"from": "USD", "to": "JPY", "rate": 148.50},
+    {"from": "JPY", "to": "USD", "rate": 0.0067},
+
+    {"from": "INR", "to": "EUR", "rate": 0.011},
+    {"from": "EUR", "to": "INR", "rate": 89.0},
+
+    {"from": "SGD", "to": "EUR", "rate": 0.66},
+    {"from": "EUR", "to": "SGD", "rate": 1.51}
+]
