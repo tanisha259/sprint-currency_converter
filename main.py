843c67bec0b506c56714bedeaa560255f83627a9
Tanis <tanishapandit0@gmail.com>
2025-12-24 14:11:38 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from exchange_rates import exchange_rates
+
+app = FastAPI(title="CurrencyConverterWebService")
+
+templates = Jinja2Templates(directory="templates")
+
+# Serve frontend
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Read Exchange Rate
+def get_exchange_rate(from_currency, to_currency):
+    for row in exchange_rates:
+        if row["from"] == from_currency and row["to"] == to_currency:
+            return row["rate"]
+    return None
+
+# API 1: Exchange Rate Lookup
+@app.get("/rate")
+def exchange_rate(fromCurrency: str, toCurrency: str):
+    rate = get_exchange_rate(fromCurrency, toCurrency)
+
+    if rate is None:
+        raise HTTPException(status_code=404, detail="Rate not found")
+
+    return {
+        "from": fromCurrency,
+        "to": toCurrency,
+        "rate": rate
+    }
+
+# API 2: Currency Conversion
+@app.get("/convert")
+def convert_currency(fromCurrency: str, toCurrency: str, amount: float):
+    rate = get_exchange_rate(fromCurrency, toCurrency)
+
+    if rate is None:
+        raise HTTPException(status_code=404, detail="Rate not found")
+
+    converted_amount = amount * rate
+
+    return {
+        "from": fromCurrency,
+        "to": toCurrency,
+        "amount": amount,
+        "convertedAmount": converted_amount
+    }
